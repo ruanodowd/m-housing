51457e6d7d09d4bd28a71606dd78466d073d9405
ruqn <59122094+ruanodowd@users.noreply.github.com>
2024-02-03 21:31:55 +0000

    renamed headings

--- mhousingsite/asgi.py
+++ mhousingsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for meowhousing project.
+ASGI config for mhousingsite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meowhousing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mhousingsite.settings')
 
 application = get_asgi_application()
--- mhousingsite/urls.py
+++ mhousingsite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for meowhousing project.
+URL configuration for mhousingsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("", include("meowapp.urls")),
+    path("", include("mhousingapp.urls")),
     path("admin/", admin.site.urls),
 ]
